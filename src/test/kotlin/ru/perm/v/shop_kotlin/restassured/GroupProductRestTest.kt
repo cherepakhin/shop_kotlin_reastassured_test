a8eb35fb7e89ad71cd816cdffdcbc4a28722c57f
Черепахин Василий <vasi.che@gmail.com>
2023-06-19 16:27:23 +0500

    add GroupProductRestTest.kt

--- src/test/kotlin/ru/perm/v/shop_kotlin/restassured/GroupProductRestTest.kt
+++ src/test/kotlin/ru/perm/v/shop_kotlin/restassured/GroupProductRestTest.kt
@@ -1,5 +1,6 @@
 package ru.perm.v.shop_kotlin.restassured
 
+import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.qameta.allure.Epic
 import io.restassured.RestAssured.given
@@ -14,6 +15,8 @@ import kotlin.test.assertEquals
 @DisplayName("Group products tests")
 class GroupProductRestTest {
 
+    val mapper = ObjectMapper()
+
     @Test
     @Epic("REST API getAll()")
     @DisplayName("GET Request GroupProductRest.all() with message is status=200")
@@ -24,55 +27,29 @@ class GroupProductRestTest {
 
     @Test
     @Epic("REST API getAll()")
-    @DisplayName("GET Request GroupProductRest.all() check dtos")
+    @DisplayName("TEST GET Request GroupProductRest.all() check dtos")
     fun getAll_check_DTO() {
-        given().`when`().get(CONSTS.GROUP_PATH).then()
-            .statusCode(HttpStatus.SC_OK)
+        val json: String = given().get(CONSTS.GROUP_PATH).asString()
+        val groups: List<GroupProductDTO> = mapper.readValue(json, object : TypeReference<List<GroupProductDTO>>() {})
+        assertEquals(6, groups.size)
+
+        val g0 = groups.get(0)
+        assertEquals(1, g0.id)
+        assertEquals(GroupProductDTO(1, "IT products", true, -1), groups.get(0))
+        assertEquals(GroupProductDTO(2, "Computers", true, 1), groups.get(1))
+        assertEquals(GroupProductDTO(3, "Desktop Computers", false, 2), groups.get(2))
     }
 
     @Test
     @Epic("Example")
     @DisplayName("Example convert array to json and assert DTO")
     fun exampleConvertJsonToGroupProductDTO() {
+        val response: Response = given().get(CONSTS.GROUP_PATH)
 
-//        val json="[{\"id\":1,\"name\":\"IT products\",\"parentId\":-1,\"isLast\":false},{\"id\":1,\"name\":\"IT products\",\"parentId\":-1,\"isLast\":false}]"
-        val json="[{\"id\":1,\"name\":\"IT products\",\"parentId\":-1},{\"id\":1,\"name\":\"IT products\",\"parentId\":-1}]"
-
-//        val response = get("/artwork");
-        val httpRequest = given()
-        val response: Response = httpRequest.get(CONSTS.GROUP_PATH)
         val jsonPathEvaluator = response.jsonPath()
-//        val names:List<String> = jsonPathEvaluator.getList<String>("name")
-//        assertEquals(6, names.size) // check count names
-        val groups:List<GroupProductDTO> = jsonPathEvaluator.getList<GroupProductDTO>(".", GroupProductDTO::class.java);
-//        val groups = jsonPathEvaluator.getList(".", List::class.java);
-//        assertEquals(6, groups.size)
-//        for (g in groups ) {
-//            println(g)
-//        }
-//        given().`when`().get(CONSTS.GROUP_PATH).`as`<GroupProductDTO>()
-//            .statusCode(HttpStatus.SC_OK)
-
-//        class DtoForTest {
-//            val id: Long = -1
-//            val name: String = ""
-//            val parentId: Long = -1
-//
-//            constructor() {
-//            }
-//        }
-
-//        val mapper:ObjectMapper = ObjectMapper()
-//        val groups = mapper.readValue(json, Array<GroupProductDTO>::class.java)
-//        mapper.readValue(jsonString)
-//
-//        val readValues: Collection<GroupProductDTO> = mapper.readValue(
-//            jsonString, object : TypeReference<Collection<GroupProductDTO?>?>() {}
-//        )
-//        val jsonPathEvaluator = get(CONSTS.GROUP_PATH).jsonPath();
-////        val groups=jsonPathEvaluator.getList(".", List::class.java) // from Object value (token `JsonToken.START_OBJECT`)
-//        val groups=jsonPathEvaluator.getList(".", TypeReference<Collection<GroupProductDTO?>?>())
-//        assertEquals(2, groups.size)
+        val groups = jsonPathEvaluator.getList<GroupProductDTO>(".", GroupProductDTO::class.java);
+        assertEquals(6, groups.size)
+        assertEquals(GroupProductDTO(1, "IT products", true, -1), groups.get(0))
     }
 
 }
--- src/test/kotlin/ru/perm/v/shop_kotlin/restassured/dto/GroupProductDTO.kt
+++ src/test/kotlin/ru/perm/v/shop_kotlin/restassured/dto/GroupProductDTO.kt
@@ -1,11 +1,35 @@
 package ru.perm.v.shop_kotlin.restassured.dto
 
-data class GroupProductDTO(
+class GroupProductDTO(
     val id: Long = -1,
     val name: String = "",
+    val haveChilds: Boolean = false,
     val parentId: Long = -1
-//    val isLast: Boolean = false
 ) {
-    constructor() : this(-1, "", -1) {
+
+    override fun toString(): String {
+        return "GroupProductDTO(id=$id, name='$name', parentId=$parentId, haveChilds=$haveChilds)"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as GroupProductDTO
+
+        if (id != other.id) return false
+        if (name != other.name) return false
+        if (parentId != other.parentId) return false
+        if (haveChilds != other.haveChilds) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + name.hashCode()
+        result = 31 * result + parentId.hashCode()
+        result = 31 * result + haveChilds.hashCode()
+        return result
     }
 }
