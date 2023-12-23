f892e3a391143e06de4129b2dc77330e4a37365a
Черепахин Василий <vasi.che@gmail.com>
2023-12-24 00:57:23 +0500

    val HOST = "http://127.0.0.1:8980/shop_kotlin/api"

--- src/test/kotlin/ru/perm/v/shop_kotlin/restassured/CONSTS.kt
+++ src/test/kotlin/ru/perm/v/shop_kotlin/restassured/CONSTS.kt
@@ -2,7 +2,7 @@ package ru.perm.v.shop_kotlin.restassured
 
 class CONSTS {
     companion object {
-        val HOST = "http://127.0.0.1:8780/api"
+        val HOST = "http://127.0.0.1:8980/shop_kotlin/api"
         val ECHO_PATH = HOST + "/echo/"
         val GROUP_PATH = HOST + "/group_product/"
         val PRODUCT_PATH = HOST + "/product/"
