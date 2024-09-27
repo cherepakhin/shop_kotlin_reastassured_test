d47bfc7279f09af1bfdd547f582270f830dc778e
Черепахин Василий <vasi.che@gmail.com>
2024-09-27 17:27:07 +0500

    add LogRestTest.kt

--- src/test/kotlin/ru/perm/v/shop_kotlin/restassured/LogRestTest.kt
+++ src/test/kotlin/ru/perm/v/shop_kotlin/restassured/LogRestTest.kt
@@ -1,13 +1,17 @@
 package ru.perm.v.shop_kotlin.restassured
 
+import io.qameta.allure.Epic
 import io.restassured.RestAssured
 import org.apache.http.HttpStatus
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 @DisplayName("Log tests /log")
 class LogRestTest {
+
     @Test
+    @Epic("REST API Log")
     @DisplayName("GET log is status=200")
     fun getLog_HttpStatusIsOK() {
         RestAssured.given().`when`().get(CONSTS.LOG_PATH).then()
