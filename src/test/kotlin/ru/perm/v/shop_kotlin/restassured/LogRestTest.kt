f45716f4056a4ac161bd5e959c418f7774f11d6c
Черепахин Василий <vasi.che@gmail.com>
2024-10-30 14:44:14 +0500

    add LogRestTest.kt

--- src/test/kotlin/ru/perm/v/shop_kotlin/restassured/LogRestTest.kt
+++ src/test/kotlin/ru/perm/v/shop_kotlin/restassured/LogRestTest.kt
@@ -3,9 +3,9 @@ package ru.perm.v.shop_kotlin.restassured
 import io.qameta.allure.Epic
 import io.restassured.RestAssured
 import org.apache.http.HttpStatus
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
 
 @DisplayName("Log tests /log")
 class LogRestTest {
