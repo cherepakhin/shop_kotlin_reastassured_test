ee4431a711adf0dc8ae4eca8aa5fb3a8a654e4e7
Черепахин Василий <vasi.che@gmail.com>
2025-01-07 16:19:22 +0500

    change @DisplayName for request echo rest

--- src/test/kotlin/ru/perm/v/shop_kotlin/restassured/EchoRestTest.kt
+++ src/test/kotlin/ru/perm/v/shop_kotlin/restassured/EchoRestTest.kt
@@ -10,20 +10,21 @@ import org.junit.jupiter.api.Test
 
 @DisplayName("Echo tests")
 class EchoRestTest {
-    val MESSAGE = "MESSAGE"
 
     @Test
     @Epic("REST API Echo")
-    @DisplayName("GET Request with message is status=200")
+    @DisplayName("GET /echo/ with message. Check status is 200.")
     fun getMessage_HttpStatusIsOK() {
+        val MESSAGE = "MESSAGE"
         given().`when`().get(CONSTS.ECHO_PATH + MESSAGE).then()
             .statusCode(HttpStatus.SC_OK)
     }
 
     @Test
     @Epic("REST API Echo")
-    @DisplayName("GET Request with message is status=200")
+    @DisplayName("GET /echo/ with message. Check message.")
     fun getMessage_CheckMessage() {
+        val MESSAGE = "MESSAGE"
         val response = get(CONSTS.ECHO_PATH + MESSAGE)
         val responseBody = response.asString()
         assertEquals(MESSAGE, responseBody)
